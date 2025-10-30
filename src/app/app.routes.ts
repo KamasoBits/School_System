bccdaf75b2c25068743cfac2e15cf7c1acac4c2f
KamasoBits <kitkatkamaso@gmail.com>
2025-10-30 16:23:33 +0300

    ratifications on the router link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
+import { HomeComponent } from './Components/home/home.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { LoginComponent } from './Components/login/login.component';
+import { StaffComponent } from './Components/staff/staff.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: LandingPageComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'staff', component: StaffComponent },
+    { path: 'student', component: HomeComponent },
+    { path: 'login', component: LoginComponent }
+
+];
